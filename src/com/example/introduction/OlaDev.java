fe6948f4edb09dbb41938095ee28448ef8fb5ef3
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-06-21 00:49:10 -0300

    create a first application in java

--- src/com/example/introduction/OlaDev.java
+++ src/com/example/introduction/OlaDev.java
@@ -1,8 +1,18 @@
 package com.example.introduction;
 
 
+import java.util.Scanner;
+
 public class OlaDev {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Qual o seu nome?");
+        String name;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nome: ");
+        name = sc.nextLine();
+
+        System.out.println("Seu nome é: " + name);
     }
 }
